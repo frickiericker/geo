efa6d984e0209618a2d8d3c61b48f05101b295fe
snsinfu <snsinfu@gmail.com>
2018-09-08 04:08:03 +0900

    Add test skeleton

---
+++ tests/point_test.cc
@@ -0,0 +1,11 @@
+// Copyright snsinfu 2018.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include "catch.hpp"
+#include "../include/geo.hpp"
+
+
+TEST_CASE("vector")
+{
+}
